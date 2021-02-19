64c1e027541bd2abdd3b0d752e993e078dbb78a6
Andrii Soloviov <as@doozer.de>
2021-02-19 11:00:41 +0100

    init server

---
+++ server/cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Handler struct{}
+
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.Index(w, r)
+}
+
+func (h Handler) Index(w http.ResponseWriter, r *http.Request) {
+	fd, err := os.Open("../../client/dist/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer fd.Close()
+	io.Copy(w, fd)
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("../../client/dist"))
+	http.Handle("/", http.StripPrefix("/", fs))
+
+	log.Println("Starting.......")
+	log.Fatal(http.ListenAndServe(":4200", nil))
+}
