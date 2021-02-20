a664eb5bd0ef053d07e45c80874f3454a67f524a
Andrii Soloviov <as@doozer.de>
2021-02-20 10:03:51 +0100

    added connection to mysql

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"be/service"
+	"be/store"
 	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	dbConn := "test:test@(localhost:3306)/test"
+	db, err := store.New(dbConn)
+	if err != nil {
+		log.Fatalf("connecting to database: %v", err)
+	}
+	_ = db
+
 	//fs := http.FileServer(http.Dir("../../client/dist"))
 	//http.Handle("/", http.StripPrefix("/", fs))
 	//go func() {log.Fatal(http.ListenAndServe(":3300", http.FileServer(http.Dir("../../client/dist"))))}()
