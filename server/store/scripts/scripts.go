72c2322a8d36c9a57a7e7167fd2693133dfae350
Andrii Soloviov <as@doozer.de>
2021-03-01 13:33:58 +0100

    update list of scripts

--- server/store/scripts/scripts.go
+++ server/store/scripts/scripts.go
@@ -13,6 +13,11 @@ func Executed() []Script {
 
 // ToExecute contains of scripts that should be executed by next deployment.
 func ToExecute() []Script {
+	return []Script{}
+}
+
+// Init is list of scripts needed to be run for restart database.
+func Init() []Script {
 	return []Script{
 		DropTableUser(),
 		CreateTableUser(),
