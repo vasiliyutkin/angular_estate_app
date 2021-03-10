ec99513b70c5c10169895fd26c5e770252377195
Andrii Blinov <ablinov@brightgrove.com>
2021-03-10 23:00:05 +0200

    project sync commit

--- client/src/app/config/social.auth.config.ts
+++ client/src/app/config/social.auth.config.ts
@@ -5,25 +5,32 @@ import {
   VKLoginProvider,
 } from 'angularx-social-login';
 
+const providers: any = [
+  [
+    {
+      id: GoogleLoginProvider.PROVIDER_ID,
+      provider: new GoogleLoginProvider(
+        '676672408690-ivu0fh2q74e139bfh90u6qrtp8i50u28.apps.googleusercontent.com'
+      ),
+    },
+    {
+      id: FacebookLoginProvider.PROVIDER_ID,
+      provider: new FacebookLoginProvider('926383761525270'),
+    },
+  ],
+];
+
+if (!!window[VKLoginProvider.PROVIDER_ID]) {
+  providers.push({
+    id: VKLoginProvider.PROVIDER_ID,
+    provider: new VKLoginProvider('7786037'),
+  });
+}
+
 export const SOCIAL_AUTH_CONFIG = {
   provide: 'SocialAuthServiceConfig',
   useValue: {
     autoLogin: false,
-    providers: [
-      {
-        id: GoogleLoginProvider.PROVIDER_ID,
-        provider: new GoogleLoginProvider(
-          '676672408690-ivu0fh2q74e139bfh90u6qrtp8i50u28.apps.googleusercontent.com'
-        ),
-      },
-      {
-        id: FacebookLoginProvider.PROVIDER_ID,
-        provider: new FacebookLoginProvider('926383761525270'),
-      },
-      {
-        id: VKLoginProvider.PROVIDER_ID,
-        provider: new VKLoginProvider('7786037'),
-      },
-    ],
+    providers,
   } as SocialAuthServiceConfig,
 };
