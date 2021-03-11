83c2e962650570b33fbd4c88a0a11f4f691e95c5
Andrii Blinov <ablinov@brightgrove.com>
2021-03-11 03:18:32 +0200

    project sync commit

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
+import { User } from '../models/user';
+import { SocialUser } from 'angularx-social-login';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
   constructor(private authService: AuthenticationService) {}
 
-  public get user() {
-    return this.authService.userValue;
+  public get user(): User | SocialUser {
+    return this.authService.userValue || this.authService.socialUserValue;
   }
-
-  ngOnInit(): void {}
 }
