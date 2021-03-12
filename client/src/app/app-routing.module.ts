505418469bca20dab378534e0e62d4fde66b7f1a
Andrii Blinov <ablinov@brightgrove.com>
2021-03-12 02:56:27 +0200

    project sync commit

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -38,7 +38,7 @@ const routes: Routes = [
       {
         path: 'about',
         component: AboutComponent,
-        canActivate: [AdminGuard],
+        // canActivate: [AdminGuard],
       },
     ],
   },
